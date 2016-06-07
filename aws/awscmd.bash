e1378630085993a0182c7c0f06fc30984d831f52
Morten Jagd Christensen <mortenjc@jcaps.com>
2016-06-07 13:24:11 +0200

    added ec2 functions

--- aws/awscmd.bash
+++ aws/awscmd.bash
@@ -9,17 +9,23 @@ function argcheck()
    fi
 }
 
+
 function helptext()
 {
    echo "s3 commands"
-   echo "  s3dir    bucket  	list entries in bucket"
-   echo "  s3list		lists the users buckets"
+   echo "  s3dir    bucket      list entries in a bucket"
+   echo "  s3list               lists the user\'s buckets"
    echo "  s3create bucket      creates a new bucket" 
    echo "  s3delete bucket      deletes bucket with content"
    echo 
    echo "ec2 commands"
+   echo "  ec2list              list available instances"
+   echo "  ec2run               list available instances"
+   echo "  ec2stop instance     stop a running instance"
+   echo "  ec2delete instance   delete a stopped instance"
 
 }
+
 
 function s3commands()
 {
@@ -54,14 +60,43 @@ exit
 }
 
 
-#run-instances --image-id "ami-d0f89fb9" --key-name mjctestkeypair --instance-type t1.micro --security-groups quicklaunch-1 
-#aws ec2 describe-instances
+function ec2commands()
+{
+case $awscmd in
 
+"ec2list")
+  aws ec2 describe-instances
+  ;;
+
+"ec2run")
+  argcheck $arg2
+  aws ec2 run-instances --image-id ami-f5f41398 --count 1 --key-name $arg2  --instance-type t2.micro --subnet-id subnet-7889a045
+  ;;
+
+"ec2stop")
+  argcheck $arg
+  aws ec2 stop-instances --instance-ids $arg
+  ;;
+
+"ec2delete")
+  argcheck $arg
+  aws ec2 terminate-instances --instance-ids $arg
+  ;;
+
+esac
+exit
+}
+
+
+#
+# Script starts here
+#
 
 awscmd=$1
 arg=$2
+arg2=$3
 
 [[ $awscmd =~ s3.*  ]] && s3commands  
-[[ $awscmd =~ ec2.* ]] && ec3commands
+[[ $awscmd =~ ec2.* ]] && ec2commands
 
 helptext
