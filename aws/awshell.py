1fde210cbfa0091bdeedec2fc1ba05c8ee054bd7
Morten Jagd Christensen <mortenjc@jcaps.com>
2016-06-13 16:54:15 +0200

    added ec2 commands

--- aws/awshell.py
+++ aws/awshell.py
@@ -9,6 +9,10 @@ def adds3(text):
    else:
       return text
 
+def call(cmd):
+   output = os.popen(cmd).read()
+   print output
+
 
 class AWShell(cmd.Cmd):
 
@@ -16,49 +20,69 @@ class AWShell(cmd.Cmd):
    prompt = '(awshell) '
    file = None
 
-#
-#
-#
-   def do_s3list(self, line):
-      'List all buckets'
-      self.call("aws s3api list-buckets")
+   subnet="subnet-7889a045"
+   ami="ami-f5f41398"
 
-   def do_s3create(self, line):
+#
+#  S3 Commands
+#
+   def do_s3_list(self, line):
+      'List all buckets'
+      call("aws s3api list-buckets")
+
+   def do_s3_create(self, line):
       'Create an S3 bucket by name'
       bucket = line.split()[0]
-      self.call("aws s3api create-bucket --acl private --bucket %s" % (bucket))
+      call("aws s3api create-bucket --acl private --bucket %s" % (bucket))
 
-   def do_s3delete(self, line):
+   def do_s3_delete(self, line):
       'Delete an S3 bucket by name'
       bucket = line.split()[0]
-      self.call("aws s3api delete-bucket --bucket %s" % (bucket))
+      call("aws s3api delete-bucket --bucket %s" % (bucket))
 
-   def do_s3ls(self, line):
+   def do_s3_ls(self, line):
       'List contents of bucket'
       bucket = adds3(line.split()[0])
-      self.call("aws s3 ls %s" % (bucket))
+      call("aws s3 ls %s" % (bucket))
 
-   def do_s3rm(self, line):
+   def do_s3_rm(self, line):
       'Remove file from bucket'
       file = adds3(line.split()[0])
-      self.call("aws s3 rm %s" % (file))
+      call("aws s3 rm %s" % (file))
 
-   def do_s3cp(self, line):
+   def do_s3_cp(self, line):
       'Copy file from bucket'
       src = line.split()[0]
       dst = line.split()[1]
-      self.call("aws s3 cp %s %s" % (src, dst))
+      call("aws s3 cp %s %s" % (src, dst))
 
 
 #
+# EC2 Commands
 #
-#
-      
-   def call(self, cmd):
-      'Calls a (aws) command, print output'
-      output = os.popen(cmd).read()
-      print output
 
+   def do_ec2_list(self, line):
+      'List all AWS instances'
+      call("aws ec2 describe-instances")
+
+   def do_ec2_run(self, line):
+      'Run instance - specify keys'
+      keys = line.split()[0]
+      call("aws ec2 run-instances --image-id %s --count 1 --key-name %s  --instance-type t2.micro --subnet-id %s" % (ami, keys, subnet))
+
+   def do_ec2_stop(self, line):
+      'Stop a running instance by instance_id'
+      instanceid = line.split()[0]
+      call("aws ec2 stop-instances --instance-ids %s" % (instanceid))
+
+   def do_ec2_delete(self, line):
+      'Terminate instance by id'
+      instanceid = line.split()[0]
+      call("aws ec2 terminate-instances --instance-ids %s" % (instanceid))
+
+#
+# CMD behavior customization
+#
    def do_shell(self, line):
       'Run a shell command'
       output = os.popen(line).read()
