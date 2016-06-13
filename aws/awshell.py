3055f9220f63f7bbb82c1afd875a0e9e6fa25fa5
Morten Jagd Christensen <mortenjc@jcaps.com>
2016-06-13 15:51:15 +0200

    porting to cmd framework

--- aws/awshell.py
+++ aws/awshell.py
@@ -1,23 +1,68 @@
 #!/usr/bin/python
 
-import cmd, sys, subprocess
+import cmd, sys, os
+
+
+def adds3(text):
+   if not text.startswith("s3://"):
+      return "s3://" + text 
+   else:
+      return text
+
 
 class AWShell(cmd.Cmd):
+
    intro = "AWS Shell"
    prompt = '(awshell) '
    file = None
 
-   def do_s3list(self, arg):
+#
+#
+#
+   def do_s3list(self, line):
       'List all buckets'
-      subprocess.call(["aws", "s3api",  "list-buckets"])
+      self.call("aws s3api list-buckets")
 
-   def do_s3create(self, arg):
+   def do_s3create(self, line):
       'Create an S3 bucket by name'
-      subprocess.call(["aws", "s3api", "create-bucket", "--acl",  "private", "--bucket", arg])
+      bucket = line.split()[0]
+      self.call("aws s3api create-bucket --acl private --bucket %s" % (bucket))
 
-   def do_s3delete(self, arg):
+   def do_s3delete(self, line):
       'Delete an S3 bucket by name'
-      subprocess.call(["aws", "s3api", "delete-bucket", "--bucket", arg])
+      bucket = line.split()[0]
+      self.call("aws s3api delete-bucket --bucket %s" % (bucket))
+
+   def do_s3ls(self, line):
+      'List contents of bucket'
+      bucket = adds3(line.split()[0])
+      self.call("aws s3 ls %s" % (bucket))
+
+   def do_s3rm(self, line):
+      'Remove file from bucket'
+      file = adds3(line.split()[0])
+      self.call("aws s3 rm %s" % (file))
+
+   def do_s3cp(self, line):
+      'Copy file from bucket'
+      src = line.split()[0]
+      dst = line.split()[1]
+      self.call("aws s3 cp %s %s" % (src, dst))
+
+
+#
+#
+#
+      
+   def call(self, cmd):
+      'Calls a (aws) command, print output'
+      output = os.popen(cmd).read()
+      print output
+
+   def do_shell(self, line):
+      'Run a shell command'
+      output = os.popen(line).read()
+      print output
 
    def do_quit(self, arg):
       'Quit the shell'
